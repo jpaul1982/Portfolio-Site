7c65fedd6f8ab8bec44989fe1f88c5b3fa4b10f2
jamesmolnar <jpaulmolnar@gmail.com>
2025-07-03 08:37:13 -0500

    edits

--- src/components/Work/SiteGrid/works.js
+++ src/components/Work/SiteGrid/works.js
@@ -1,10 +1,6 @@
 // assets
 import WestBusinessLogo from "../../../assets/images/west-business-interiors-logo.svg";
 import ImmigrationGroupLogo from "../../../assets/images/immigration group.svg";
-// import GeminiLogo from "../../../assets/images/gemini.png";
-// import ApplewoodLogo from "../../../assets/images/Applewood-logo.png";
-import KoKLogo from "../../../assets/images/KofKLogo.png";
-// import ZubieLogo from "../../../assets/images/zubie.png";
 import ChartwellLogo from "../../../assets/images/chartwell-logo.webp";
 import FCLogo from "../../../assets/images/FClogo-compressed.2022_FC.Black_.compressed.logo_.png";
 import GenesisLogo from "../../../assets/images/Genesis-Logo-1.png"
@@ -35,7 +31,7 @@ let works = [
   },
     {
     companyName: "Fairway Lawns",
-    background: "#017840",
+    background: "#fff",
     logo: FairwayLogo,
     url: "https://fairwaylawns.com/",
   },
@@ -93,12 +89,6 @@ let works = [
     logo: GenesisLogo,
     url: "https://geneng.net/",
   },
-  // {
-  //   companyName: "GeminiAMS",
-  //   background: "#19325A",
-  //   logo: GeminiLogo,
-  //   url: "https://geminiams.com/",
-  // },
   {
     companyName: "NCFL",
     background: "#a5a940",
@@ -110,16 +100,6 @@ let works = [
     background: "#111111",
     logo: DDLogo,
     url: "https://diamondmobiledetailingmn.com/",
-  },
-  // {
-  //   background: "#F15D22",
-  //   logo: ApplewoodLogo,
-  //   url: "https://www.applewoodfixit.com/",
-  // },
-  {
-    background: "#00467F",
-    logo: KoKLogo,
-    url: "https://www.kingofkingswoodbury.org/",
   },
   {
     companyName: "Element 47",
@@ -133,12 +113,6 @@ let works = [
     logo: MSMALogo,
     url: "https://www.msmachisago.com/",
   },
-  // {
-  //   companyName: "Zubie",
-  //   background: "#00C2DF",
-  //   logo: ZubieLogo,
-  //   url: "https://zubie.com/",
-  // }
 ];
 
 const makeKeys = () => {
